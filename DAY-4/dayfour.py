6f0ca753ccaa4ae6dbae132703b4cb01b7fec711
R Ramprasad <127487291+VANTHIYATHEVANN@users.noreply.github.com>
2024-01-29 11:57:10 +0530

    Update dayfour.py.

--- DAY-4/dayfour.py
+++ DAY-4/dayfour.py
@@ -1,8 +1,11 @@
 f=open('inputfile.txt')
 su = 0
-N = {}
+dic={}
 i=0
 for line in f:
+    if i not in dic:
+        dic[i]=0
+    dic[i]+=1
     matches = line.split('|')
     first_match=matches[0].split(':')
     first = [int(x) for x in first_match[1].split()]
@@ -15,4 +18,13 @@ for line in f:
     count = len(common)
     if count > 0:
         su += 2**(count-1)
+    for j in range(count):
+        if i+j+1 not in dic:
+            dic[i+j+1]=0
+        dic[i+j+1]+=dic[i]
+    i+=1
 print("Sum is:",su)
+s=0
+for key in dic.keys():
+    s+=dic[key]
+print("Part 2:",s)
