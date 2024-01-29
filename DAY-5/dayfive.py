2529bfc25adbd5e28a1e737dab9c4e33a01c32cc
R Ramprasad <127487291+VANTHIYATHEVANN@users.noreply.github.com>
2024-01-29 12:55:54 +0530

    Update dayfive.py.

--- DAY-5/dayfive.py
+++ DAY-5/dayfive.py
@@ -1,3 +1,4 @@
+import math
 f=open('inputfile.txt')
 almanac=[]
 for line in f:
@@ -23,7 +24,7 @@ for line in almanac[2:]:
             temp = line.split()
             all_maps[-1].append(func(line))
             process_line = True
-min=100000
+mi=1000000000000000000000
 for seed in seeds:
     loc=seed
     for val in all_maps:
@@ -31,6 +32,39 @@ for seed in seeds:
             if source <= loc < source + rangelen:
                 loc = destination + (loc - source)
                 break
-    if loc<min:
-        min=loc
-print(min)
+    if loc<mi:
+        mi=loc
+print("Part 1:",mi)
+
+file_content = open('inputfile.txt').read()
+sections = file_content.split("\n\n")
+inputs = sections[0]
+blocks = sections[1:]
+inputs = list(map(int, inputs.split(":")[1].split()))
+seeds = []
+for i in range(0, len(inputs), 2):
+    seeds.append((inputs[i], inputs[i] + inputs[i + 1]))
+for block in blocks:
+    ranges = []
+    for line in block.splitlines()[1:]:
+        temp=[]
+        for num in line.split():
+            temp.append(int(num))
+        ranges.append(temp)
+    new_seeds = []
+    while len(seeds) > 0:
+        s, e = seeds.pop()      
+        for a, b, c in ranges:
+            os = max(s, b)
+            oe = min(e, b + c)
+            if os < oe:
+                new_seeds.append((os - b + a, oe - b + a))
+                if os > s:
+                    seeds.append((s, os))
+                if e > oe:
+                    seeds.append((oe, e))
+                break
+        else:
+            new_seeds.append((s, e))
+    seeds = new_seeds
+print("Part 2:",min(seeds)[0])
